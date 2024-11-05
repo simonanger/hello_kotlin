47d9067dbcc06ecee1b63657e330df1b023a3fc5
Simon Anger <simonanger89@gmail.com>
2024-11-05 15:40:42 +0000

    Finished part 4 chapter 7 - abstract classes and interfaces

--- src/example/myapp/main.kt
+++ src/example/myapp/main.kt
@@ -1,33 +1,16 @@
 package example.myapp
 
-fun buildAquarium() {
-    //everything default
-    val aquarium1 = Aquarium()
-    aquarium1.printSize()
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
 
-    // default height and length
-    val aquarium2 = Aquarium(width = 25)
-    aquarium2.printSize()
-
-    // default width
-    val aquarium3 = Aquarium(height = 35, length = 110)
-    aquarium3.printSize()
-
-    //everything custom
-    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
-    aquarium4.printSize()
-
-    val aquarium5 = Aquarium(numberOfFish = 29)
-    aquarium5.printSize()
-
-    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
-    aquarium6.printSize()
-
-    val myTower = TowerTank(diameter = 25, height = 40)
-    myTower.printSize()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 
 }
 
 fun main() {
-    buildAquarium()
+    makeFish()
 }
