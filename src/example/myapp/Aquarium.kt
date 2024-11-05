81c75e770f0fea0e6ac3d1775b60c7e39a4b85a7
Simon Anger <simonanger89@gmail.com>
2024-11-05 13:59:48 +0000

    Finished part 4 chapter 6 - subclasses and inheritance

--- src/example/myapp/Aquarium.kt
+++ src/example/myapp/Aquarium.kt
@@ -1,4 +1,49 @@
 package example.myapp
+import java.lang.Math.PI
 
-class Aquarium {
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
+    init {
+        println("aquarium initialising")
+    }
+
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+    open var volume: Int
+        get() = width * height * length / 1000 // 1000 cm^3 = 1 l
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesnt spill
+        val tank = numberOfFish * 2000 * 1.1
+
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
+
+    fun printSize() {
+        println(shape)
+        println("Width: $width cm" +
+                " Length: $length cm" +
+                " Height: $height cm")
+
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l Water: $water ; (${water/volume*100.0}% full)")
+    }
+}
+
+class TowerTank (override var height: Int, var diameter: Int) : Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+    // ellipse area = pi
+    get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+
+    override var water = volume * 0.8
+    override  val shape = "cylinder"
 }
