a625c8961e69eb174272bccd0bd918f3357930e9
Simon Anger <simonanger89@gmail.com>
2024-11-06 18:08:15 +0000

    Finished part 4

--- src/example/myapp/AquariumFish.kt
+++ src/example/myapp/AquariumFish.kt
@@ -28,3 +28,14 @@ class PrintingFishAction(val food: String) : FishAction {
         println(food)
     }
 }
+
+sealed class Seal
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal): String {
+    return when(seal) {
+        is Walrus -> "walrus"
+        is SeaLion -> "sealion"
+    }
+}
--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -15,6 +15,16 @@ fun makeDecorations() {
     println(diver)
 }
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 fun main() {
-    makeDecorations()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
