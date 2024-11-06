7133cc7b2aa74434d98f87811fb28f9fdcd93945
Simon Anger <simonanger89@gmail.com>
2024-11-06 17:34:20 +0000

    Finished part 4 chapter 8 - interface delegation

--- src/example/myapp/AquariumFish.kt
+++ src/example/myapp/AquariumFish.kt
@@ -1,23 +1,30 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
+interface FishAction {
+    fun eat()
 }
 
-class Shark: AquariumFish(), FishAction {
+interface FishColor {
+    val color: String
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class Shark: FishAction, FishColor {
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction {
-    override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
-}
+class Plecostomus(fishColor: FishColor = GoldColor):
+        FishAction by PrintingFishAction("eat algae"),
+        FishColor by fishColor
 
-interface FishAction {
-    fun eat()
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
 }
